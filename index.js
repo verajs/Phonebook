6d44d4af69274c992de5ed48f073eba5001aea6f
Sebastian Vera <100543895+verajs@users.noreply.github.com>
2022-04-18 21:06:16 -0400

    paths added

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const persons = [
-    { 
-      "id": 1,
-      "name": "Aleksandr Scriabin", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Yerin Baek", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Satoshi Nakamoto", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Larry David", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = [
+  {
+    id: 1,
+    name: "Aleksandr Scriabin",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Yerin Baek",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Satoshi Nakamoto",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Larry David",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/', (request, response) => { 
-    response.send('<h1>Welcome to the Phonebook API!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Welcome to the Phonebook API!</h1>");
+});
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+app.get("/info", (request, response) => {
+  const date = new Date();
+  const length = persons.length;
+  response.send(
+    `<p>Phonebook has info for ${length} people</p> <p>${date}</p>`
+  );
+});
 
-const PORT = 3001
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
+  console.log(`Server running on port ${PORT}`);
+});
